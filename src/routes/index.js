2b6700e5016453e14f485b83b128e2f3b461b065
Tapachula <kentoworkspace24@gmail.com>
2026-09-18 16:06:16 -0600

    feat: add public categories endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { categoriesRouter } from "./categories.routes.js";
 import { healthRouter } from "./health.routes.js";
 
 export const apiRouter = Router();
 
 apiRouter.use("/health", healthRouter);
+apiRouter.use("/categories", categoriesRouter);
